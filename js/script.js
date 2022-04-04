222d686c9d37a21f5991813ab98c660a6c5c555b
Edward.M <99274690+Eddie-Mcnamara@users.noreply.github.com>
2022-04-04 15:07:21 +0000

    2added variables

--- js/script.js
+++ js/script.js
@@ -1,48 +1,34 @@
-window.onload = () => {
-    let button = document.querySelector("#btn");
+
   
-    // Function for calculating BMI
-    button.addEventListener("click", calculateBMI);
-};
+'use strict'
+/**
+ * This function calculates tax based on your hours worked and hourly salary
+ */
+function calculate () {
+  // input
+  let height = parseFloat(document.getElementById('height').value)
+  let weight = parseFloat(document.getElementById('weight').value) 
+
+  // process
+  const CONVERSION_RATE = 0.10
+  let heightm = height * CONVERSION_RATE
+  let bmi = weight / heightm ** 2
+
   
-function calculateBMI() {
+
+  // Dividing as per the bmi conditions
+    //     if (bmi < 18.6) result.innerHTML =
+    //         `Under Weight : <span>${bmi}</span>`;
   
-    /* Getting input from user into height variable.
-    Input is string so typecasting is necessary. */
-    let height = parseInt(document
-            .querySelector("#height").value);
+    //     else if (bmi >= 18.6 && bmi < 24.9) 
+    //         result.innerHTML = 
+    //             `Normal : <span>${bmi}</span>`;
   
-    /* Getting input from user into weight variable. 
-    Input is string so typecasting is necessary.*/
-    let weight = parseInt(document
-            .querySelector("#weight").value);
-  
-    let result = document.querySelector("#result");
-  
-    // Checking the user providing a proper
-    // value or not
-    if (height === "" || isNaN(height)) 
-        result.innerHTML = "Provide a valid Height!";
-  
-    else if (weight === "" || isNaN(weight)) 
-        result.innerHTML = "Provide a valid Weight!";
-  
-    // If both input is valid, calculate the bmi
-    else {
-  
-        // Fixing upto 2 decimal places
-        let bmi = (weight / ((height * height) 
-                            / 10000)).toFixed(2);
-  
-        // Dividing as per the bmi conditions
-        if (bmi < 18.6) result.innerHTML =
-            `Under Weight : <span>${bmi}</span>`;
-  
-        else if (bmi >= 18.6 && bmi < 24.9) 
-            result.innerHTML = 
-                `Normal : <span>${bmi}</span>`;
-  
-        else result.innerHTML =
-            `Over Weight : <span>${bmi}</span>`;
-    }
-}
+    //     else result.innerHTML =
+    //         `Over Weight : <span>${bmi}</span>`;
+    // }
+        
+ // output
+  document.getElementById('bmi').innerHTML = 'The governement will take' + ' $' + bmi
+   
+ 

2b8110c63e7286405ef475265212ec63a6b26312
Edward.M <99274690+Eddie-Mcnamara@users.noreply.github.com>
2022-04-05 15:04:28 +0000

    added _______

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+// Copyright (c) 2022 Edward Mcnamara All rights reserved
+//
+// Created by: Edward Mcnamara
+// Created on: March 2022
+// This file contains the JS functions for index.html
 
-  
 'use strict'
 /**
  * This function calculates tax based on your hours worked and hourly salary
@@ -10,25 +14,24 @@ function calculate () {
   let weight = parseFloat(document.getElementById('weight').value) 
 
   // process
-  const CONVERSION_RATE = 0.10
-  let heightm = height * CONVERSION_RATE
-  let bmi = weight / heightm ** 2
+  const CONVERSION_RATE = 0.01
+  let heightConverted = height * CONVERSION_RATE
+  let bmi = weight / heightConverted **2
 
-  
 
-  // Dividing as per the bmi conditions
-    //     if (bmi < 18.6) result.innerHTML =
-    //         `Under Weight : <span>${bmi}</span>`;
+   // //Dividing as per the bmi conditions
+   //       if (bmi < 18.6) result.innerHTML =
+   //          `Under Weight : <span>${bmi}</span>`;
   
-    //     else if (bmi >= 18.6 && bmi < 24.9) 
-    //         result.innerHTML = 
-    //             `Normal : <span>${bmi}</span>`;
+   //       else if (bmi >= 18.6 && bmi < 24.9) 
+   //      result.innerHTML = 
+   //              `Normal : <span>${bmi}</span>`;
   
-    //     else result.innerHTML =
-    //         `Over Weight : <span>${bmi}</span>`;
-    // }
-        
- // output
-  document.getElementById('bmi').innerHTML = 'The governement will take' + ' $' + bmi
-   
- 
+   //       else result.innerHTML =
+   //        `Over Weight : <span>${bmi}</span>`;
+   //   }
+
+  // output
+  document.getElementById('bmi').innerHTML = 'Your BMI is ' + bmi.toFixed(2)
+  
+}
